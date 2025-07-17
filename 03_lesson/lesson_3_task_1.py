582fde43ca493cddff84ea6aa58be8ebf23dcdfc
Sonya-truba <sonya.truba@gmail.com>
2025-07-17 14:17:36 +0300

    Второй урок. Вторая отправка.

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,6 @@
+from user import User
+
+my_user = User("Sonya", "Trubitsina")
+my_user.myName()
+my_user.myLastName()
+my_user.myFullName()
---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,11 @@
+from smartphone import Smartphone
+catalog = [
+    Smartphone("IPhone", "16Pro", "+71234556677"),
+    Smartphone("IPhone", "16ProMa[", "+71234556688"),
+    Smartphone("Sumsung", "23S", "+71234556699"),
+    Smartphone("Sumsung", "24SE", "+71234558877"),
+    Smartphone("Sumsung", "22FE", "+71234552233"),
+]
+
+for smartphone in catalog:
+    print(f"{smartphone.mark} - {smartphone.model}. {smartphone.number}")
